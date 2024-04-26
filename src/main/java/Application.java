1bd4de51f23b91dd8b056ccd9134004144450f58
meryemmacar <meryemmacaroglu13@gmail.com>
2024-04-26 18:21:57 +0300

    5

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,18 +1,5 @@
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
 
-
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
     }
 }
